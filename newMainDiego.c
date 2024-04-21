5394c029156849a44817dd8d64578106259ef1b5
Diego <50855496+getgudbud@users.noreply.github.com>
2024-04-21 11:10:52 -0700

    Update newMainDiego.c

--- newMainDiego.c
+++ newMainDiego.c
@@ -19,12 +19,21 @@
  *       It does not necessarily mean that the buffer is full.
  * sandwich_sem: initialized to the size of 8 and used to check if there are sandwhich slots left in buffer
  */
+
+typedef struct {
+    unsigned int sleep_time;
+    unsigned int production_max;
+    ConsumerType type;
+} ConsumerArgs;
+
 sem_t mutex, empty, full, sandwich_sem, barrier;
 pthread_cond_t pizza_produced = PTHREAD_COND_INITIALIZER;
 pthread_cond_t sandwich_produced = PTHREAD_COND_INITIALIZER;
 std::queue<RequestAdded> buffer; // Buffer to hold items
 unsigned int inBrokerQueue[RequestTypeN];
 unsigned int produced[RequestTypeN];
+unsigned int consumed[ConsumerTypeN];
+int totalInQueue = 0;
 
 DeliveryQueue queue = {0, 0, 0, 0, 0,
                        0, {2, 2, 2, 2, 2, 2, 2, 2, 2,
@@ -64,6 +73,7 @@ void *pizza_producer(void *arg) {
         item.inBrokerQueue[Pizza]++;
         item.produced[Pizza]++;
         buffer.push(item);
+        totalInQueue++;
         log_added_request(item);
 
 
@@ -102,7 +112,8 @@ void *sandwich_producer(void *arg) {
         item.produced = produced;
         item.inBrokerQueue[Sandwich]++;
         item.produced[Sandwich]++;
-        
+        buffer.push(item);
+        totalInQueue++;
         log_added_request(item);
 
         sem_post(&sandwich_sem); //Make sure sandwhich can be inserted
@@ -115,145 +126,97 @@ void *sandwich_producer(void *arg) {
 }
 
 
-void *consumer_a(void *arg) {
-    int *args = (int *)arg;
-    int sleep_time = args[0]; // Extract the sleep time
-    int n = args[1];          // Extract the value of n
-    int producerCheck = args[2];
+// Consumer function
+void* consumer_a(void* arg) {
+    auto* args = (ConsumerArgs*)arg;
+    unsigned int sleep_time = args->sleep_time; // Extract the sleep time
+    unsigned int production_max = args->production_max; // Extract the value of n
+    ConsumerType type = args->type; // Extract the consumer type
 
     while (true) {
-        DeliveryQueue copy = queue;
+        sem_wait(&full); // Block until something to consume
+        sem_wait(&mutex); // Access buffer exclusively
 
-        sem_wait(&mutex);
-        sem_wait(&full);
-
-        if (queue.broker[0] == 0) {
-            queue.conA[0]++;
-            if ((queue.pizza_requests + queue.sandwich_requests) > 0) {
-                for (int i = 0; i < 19 && queue.broker[i] != 2; i++) {
-                    queue.broker[i] = queue.broker[i + 1];  // Shift elements to the left
-                }
-                queue.broker[19] = 2;
-            }
-
-
-            queue.pizza_requests--;
-            // Log the request removed
-            unsigned int inQueue[2] = {queue.pizza_requests, queue.sandwich_requests};
-            unsigned int totals[2] = {queue.total_pizzas, queue.total_sandwiches};
-            RequestRemoved removed = {DeliveryServiceA, Pizza, inQueue, totals};
-            log_removed_request(removed);
-            sleep_based_on_input(sleep_time);
-
-            //printf("--Removed Pizza, w/ %d in there and %d in total deliveries!! \n", queue.pizza_requests, queue.total_requests);
+        // Check if all items have been consumed
+        if (totalInQueue == 0 && produced[Pizza] >= production_max && produced[Sandwich] >= production_max) {
+            sem_post(&mutex);
             sem_post(&empty);
-        } else {
-            queue.conA[1]++;
-            if ((queue.pizza_requests + queue.sandwich_requests) > 0) {
-                for (int i = 0; i < 19; i++) {
-                    queue.broker[i] = queue.broker[i + 1];  // Shift elements to the left
-                }
-                queue.broker[19] = 2;
-            }
+            break;
+        }
 
-            queue.sandwich_requests--;
-            unsigned int inQueue[2] = {queue.pizza_requests, queue.sandwich_requests};
-            unsigned int totals[2] = {queue.total_pizzas, queue.total_sandwiches};
-            // Log the request removed
-            RequestRemoved removed = {DeliveryServiceA, Sandwich, inQueue, queue.conA};
-            log_removed_request(removed);
-            //printf("--Removed Sandwich, w/ %d in there and %d in total deliveries!!\n", queue.sandwich_requests, queue.total_requests);
-            sleep_based_on_input(sleep_time);
+        // Consume item
+        RequestRemoved item;
+        item.consumer = type; // Set consumer type
+        item.type = buffer.front().type; // Set request type
+        item.inBrokerQueue = inBrokerQueue; // Point to the global inBrokerQueue array
+        item.consumed = consumed; // Point to the global consumed array
+        item.inBrokerQueue[item.type]--;
+        item.consumed[type]++; // Increment the count of items consumed by this consumer
+        buffer.pop();
 
-            sem_post(&empty);
+
+        // If the consumed item is a sandwich, signal the sandwich semaphore
+        if (item.type == Sandwich) {
             sem_post(&sandwich_sem);
         }
 
-        int wow = queue.total_requests;
-        int wowPizza = queue.sandwich_requests;
-        int wowSandwich = queue.pizza_requests;
+        buffer.pop();
+        totalInQueue--;
+        printf("Consumed at %ld\n", time(NULL));
 
+        sem_post(&mutex); // Release exclusive access to buffer
+        sem_post(&empty); // Signal available slot
 
-        if (queue.total_requests >= n && (queue.pizza_requests + queue.sandwich_requests) == 0) {
-            sem_post(&barrier); // Signal the barrier when all requests have been consumed
-            pthread_exit(NULL);
-        }
-
-        sem_post(&mutex);
+        usleep(sleep_time * 1000); // Sleep for sleep_time ms
     }
+    return NULL;
 }
 
-void *consumer_b(void *arg) {
-    int *args = (int *)arg;
-    int sleep_time = args[0]; // Extract the sleep time
-    int n = args[1];          // Extract the value of n
-    int producerCheck = args[2];
 
 
-    while (1) {
+// Consumer function
+void* consumer_b(void* arg) {
+    auto* args = (ConsumerArgs*)arg;
+    unsigned int sleep_time = args->sleep_time; // Extract the sleep time
+    unsigned int production_max = args->production_max; // Extract the value of n
+    ConsumerType type = args->type; // Extract the consumer type
 
-        DeliveryQueue copy = queue;
+    while (true) {
+        sem_wait(&full); // Block until something to consume
+        sem_wait(&mutex); // Access buffer exclusively
 
-        sem_wait(&mutex);
-        pthread_mutex_lock(reinterpret_cast<pthread_mutex_t *>(&mutex)); // Lock the mutex
-        int sem_value;
-        sem_wait(&full);
-        if (queue.broker[0] == 0) {
-            queue.conB[0]++;
-            if ((queue.pizza_requests + queue.sandwich_requests) > 0) {
-                for (int i = 0; i < 19 && queue.broker[i] != 2; i++) {
-                    queue.broker[i] = queue.broker[i + 1];  // Shift elements to the left
-                }
-                queue.broker[19] = 2;
-            }
-
-            sem_getvalue(&full, &sem_value);
-            if (sem_value >= 20) {
-                // printf("Semaphore higher than 20!!!!!!\n");
-            } else {
-                //printf("Semaphore is good i guess... \n");
-            }
-
-
-            queue.pizza_requests--;
-            // Log the request removed
-            unsigned int inQueue[2] = {queue.pizza_requests, queue.sandwich_requests};
-            unsigned int totals[2] = {queue.total_pizzas, queue.total_sandwiches};
-            RequestRemoved removed = {DeliveryServiceB, Pizza, inQueue, queue.conB};
-            log_removed_request(removed);
-            //printf("--Removed Pizza, w/ %d in there and %d in total deliveries!! \n", queue.pizza_requests, queue.total_requests);
+        // Check if all items have been consumed
+        if (totalInQueue == 0 && produced[Pizza] >= production_max && produced[Sandwich] >= production_max) {
+            sem_post(&mutex);
             sem_post(&empty);
+            break;
+        }
 
-        } else {
-            queue.conB[1]++;
-            if ((queue.pizza_requests + queue.sandwich_requests) > 0) {
-                for (int i = 0; i < 19; i++) {
-                    queue.broker[i] = queue.broker[i + 1];  // Shift elements to the left
-                }
-                queue.broker[19] = 2;
-            }
+        // Consume item
+        RequestRemoved item;
+        item.consumer = type; // Set consumer type
+        item.type = buffer.front().type; // Set request type
+        item.inBrokerQueue = inBrokerQueue; // Point to the global inBrokerQueue array
+        item.consumed = consumed; // Point to the global consumed array
+        item.inBrokerQueue[item.type]--;
+        item.consumed[type]++; // Increment the count of items consumed by this consumer
+        buffer.pop();
 
-            queue.sandwich_requests--;
-            unsigned int inQueue[2] = {queue.pizza_requests, queue.sandwich_requests};
-            unsigned int totals[2] = {queue.total_pizzas, queue.total_sandwiches};
-            // Log the request removed
-            RequestRemoved removed = {DeliveryServiceB, Sandwich, inQueue, queue.conB};
-            log_removed_request(removed);
-            //printf("--Removed Sandwich, w/ %d in there and %d in total deliveries!!\n", queue.sandwich_requests, queue.total_requests);
-
-            // Sleep based on the sleep time provided
-            sleep_based_on_input(sleep_time);
-
-            sem_post(&empty);
+        // If the consumed item is a sandwich, signal the sandwich semaphore
+        if (item.type == Sandwich) {
             sem_post(&sandwich_sem);
         }
 
-        if (queue.total_requests >= n && (queue.pizza_requests + queue.sandwich_requests) == 0) {
-            sem_post(&barrier); // Signal the barrier when all requests have been consumed
-        }
+        buffer.pop();
+        totalInQueue--;
+        printf("Consumed at %ld\n", time(NULL));
 
-        sem_post(&mutex);
+        sem_post(&mutex); // Release exclusive access to buffer
+        sem_post(&empty); // Signal available slot
+
+        usleep(sleep_time * 1000); // Sleep for sleep_time ms
     }
+    return NULL;
 }
 
 
@@ -263,7 +226,6 @@ void *consumer_b(void *arg) {
 
 
 int main(int argc, char *argv[]) {
-    pthread_t producers[2], consumers[2];
     unsigned n = MAX_TOTAL_REQUESTS, a = 0, b = 0, p = 0, s = 0;
     int opt;
 
@@ -289,12 +251,15 @@ int main(int argc, char *argv[]) {
                 exit(EXIT_FAILURE);
         }
     }
+
+    pthread_t producers[2], consumers[2];
     // Pass sleep time and n to producer threads
     unsigned int pizza_args[2] = {p, n};
     unsigned int sandwich_args[2] = {s, n};
-    // Pass sleep time and n to consumer threads
-    unsigned int a_args[2] = {a, n};
-    unsigned int b_args[2] = {b, n};
+
+    // Pass sleep time, n, and type to consumer threads
+    ConsumerArgs a_args = {a, n, DeliveryServiceA};
+    ConsumerArgs b_args = {b, n, DeliveryServiceB};
 
     sem_init(&mutex, 1, 1);
     sem_init(&empty, 1, MAX_REQUESTS);
